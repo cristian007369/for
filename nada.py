ceec3a0118dc80d62a90ca1243c82853739a1e6e
cristian007369 <cristiankira123@gmail.com>
2023-04-22 07:06:12 -0500

    Enfermedades mucosas

---
+++ nada.py
@@ -0,0 +1,39 @@
+# ejercicio 3_simular el lanzamiento de n dados, e imprimir cuantas veces cayo cada cara . mostrar resultado con barras y asteriscos 
+import random
+#Se declaran seis variables para almacenar las veces que cae cada cara 
+#También se pide el número de veces que sera lanzado el dado
+lanzamiento=36
+cara1 = cara2 = cara3 = cara4 = cara5 = cara6=0 
+cont=0
+#Processing
+while True:
+    for i in range(lanzamiento):
+       dado=random.randint(1,6)
+       if dado==1:
+        cara1+=1
+       elif dado==2:
+        cara2+=1
+       elif dado==3:
+        cara3+=1
+       elif dado==4:
+        cara4+=1
+       elif dado==5:
+        cara5+=1
+       else:
+        cara6+=1
+    cont+=1
+    if cara1 == cara2 == cara3 == cara4 == cara5 == cara6==6 :
+      break
+
+    cara1 = cara2 = cara3 = cara4 = cara5 = cara6 = 0 
+
+
+
+#Output
+print(cont)
+print("*"*cara1+f" cara de lado 1, {cara1}\n" +
+      "*"*cara2+f" cara de lado 2, {cara2}\n" +
+      "*"*cara3+f" cara de lado 3, {cara3}\n" +
+      "*"*cara4+f" cara de lado 4, {cara4}\n" +
+      "*"*cara5+f" cara de lado 5, {cara5}\n" +
+      "*"*cara6+f" cara de lado 6, {cara6}")
